57c451417177accf889822766947cfc38c6731f3
manoj <manojadabala123@gmail.com>
2025-02-18 13:49:49 +0530

    change comment

--- assignment 4/ques8.c
+++ assignment 4/ques8.c
@@ -4,7 +4,7 @@ int main(){
     int a,b;
     printf("Enter any two integers: ");
     scanf("%d%d",&a,&b);
-    a = a+b-(b=a); // or b = a+b -(a=b); first we sum both variables now swap b to a and subtract from sum..just think from total how much we need to subtract such that  b comes into a.
+    a = a+b-(b=a); // or b = a+b -(a=b); first we sum both variables now put a into b and subtract from sum..just think from total how much we need to subtract such that  b comes into a.
     printf("Swapped,a=%d,b=%d",a,b);
     return 0;
 }
