021f3c67b5ee920d563d8ea2c354001091aa5d89
manoj <manojadabala123@gmail.com>
2025-02-18 10:56:59 +0530

    Add 3rd solution for assignment 4

---
+++ assignment 4/ques3.c
@@ -0,0 +1,9 @@
+//assign 'A' to char type variable and change it to 'A' to 'B' using increment operator.
+#include<stdio.h>
+int main(){
+    
+    char character = 'A';
+    character++;
+    printf("The next character is %c",character);
+    return 0;
+}
