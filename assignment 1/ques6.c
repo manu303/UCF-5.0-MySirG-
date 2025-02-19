637f6d407d96e378d496f75fb4cadb0010ab0909
manoj <manojadabala123@gmail.com>
2025-02-19 21:18:43 +0530

    remove \ on single quote '

--- assignment 1/ques6.c
+++ assignment 1/ques6.c
@@ -1,6 +1,6 @@
 //print "Teacher's Day" on screen along with single and double quotes.
 #include<stdio.h>
 int main(){
-    printf("\"Teacher\'s Day\"");
+    printf("\"Teacher's Day\"");
     return 0;
 }
