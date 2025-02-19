ddedec6a0f5940017537b40cd81c242626ed32e9
manoj <manojadabala123@gmail.com>
2025-02-19 21:36:52 +0530

    change avg  variable datatype

--- assignment 2/ques1.c
+++ assignment 2/ques1.c
@@ -1,10 +1,11 @@
 //average of 3 integers.
 #include<stdio.h>
 int main(){
-    int num1,num2,num3,avg;
+    int num1,num2,num3;
+    float avg;
     printf("Enter 3 numbers:");
     scanf("%d%d%d",&num1,&num2,&num3);
-    avg = (num1+num2+num3)/3;
-    printf("The average of %d,%d and %d is:%d",num1,num2,num3,avg);
+    avg = (num1+num2+num3)/3.0;
+    printf("The average of %d,%d and %d is:%0.2f",num1,num2,num3,avg);
     return 0;
 }
