37b0dbcdbda43386632e7a3c5ec771167f88949e
manoj <manojadabala123@gmail.com>
2025-02-19 21:27:27 +0530

    remove area variable

--- assignment 1/ques9.c
+++ assignment 1/ques9.c
@@ -1,10 +1,9 @@
 //calculate area of rectangle input given by user.
 #include<stdio.h>
 int main(){
-    float length,width,area;
+    float length,width;
     printf("Enter length and width:");
     scanf("%f%f",&length,&width);
-    area = length*width;
-    printf("Area of rectangle is : %.2f",area);
+    printf("Area of rectangle is : %.2f",length*width);
     return 0;
 }
