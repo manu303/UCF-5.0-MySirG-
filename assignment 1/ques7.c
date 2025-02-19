eeb2ee59c163f819200928a11c2a1e5c1fce06e2
manoj <manojadabala123@gmail.com>
2025-02-19 21:20:24 +0530

    remove sum variable

--- assignment 1/ques7.c
+++ assignment 1/ques7.c
@@ -1,10 +1,9 @@
 //calculate sum of integers,input takes from user.
 #include<stdio.h>
 int main(){
-    int num1,num2,sum;
+    int num1,num2;
     printf("Enter two numbers:");
     scanf("%d%d",&num1,&num2);
-    sum = num1+num2;
-    printf("Sum of %d and %d is: %d",num1,num2,sum);
+    printf("Sum of %d and %d is: %d",num1,num2,num1+num2);
     return 0;
 }
