886b1b1b0634f406b94aec7de6ac14283aa15104
manoj <manojadabala123@gmail.com>
2025-02-19 21:50:44 +0530

    remove unnecessary variables

--- assignment 2/ques5.c
+++ assignment 2/ques5.c
@@ -1,23 +1,9 @@
 //calculate profit and loss for selling 25 bananas.
 #include<stdio.h>
 int main(){
-    float cost_price_per_dozen,
-    selling_price_per_dozen,
-    profit,
-    loss,
-    cp_per_banana,
-    sp_per_banana;
-    printf("Enter c.p and s.p for 1 dozen of bananas:");
-    scanf("%f%f",&cost_price_per_dozen,&selling_price_per_dozen);
-    cp_per_banana = cost_price_per_dozen/12;
-    sp_per_banana = selling_price_per_dozen/12;
-    if (sp_per_banana>=cp_per_banana){
-        profit = (sp_per_banana-cp_per_banana)*25;
-        printf("Profit for selling 25 bananas is:%.2f",profit);
-    }
-    else{
-        loss=(cp_per_banana-sp_per_banana)*25;
-        printf("loss for selling 25 bananas is:%.2f",loss);
-    }
+    float cp,sp;
+    printf("Enter cost price and selling price for 1 dozen of bananas:");
+    scanf("%f%f",&cp,&sp);
+    printf("The profit or loss is : %0.2f",(sp-cp)/12*25);
     return 0;
 }
