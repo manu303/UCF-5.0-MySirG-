a9f4a0087acd3bfa55f8f98493a4df3675984d5c
manoj <manojadabala123@gmail.com>
2025-02-19 21:33:37 +0530

    remove pi variable

--- assignment 1/ques10.c
+++ assignment 1/ques10.c
@@ -1,10 +1,11 @@
 //calculate area of circle input given by user.
 #include<stdio.h>
 int main(){
-    float pi=3.14,radius,area;
+    int radius;
+    float area;
     printf("Enter radius of  a circle:");
-    scanf("%f",&radius);
-    area = pi*radius*radius;
+    scanf("%d",&radius);
+    area = 3.14*radius*radius;
     printf("The area of circle is:%.2f",area);
     return 0; 
 }
