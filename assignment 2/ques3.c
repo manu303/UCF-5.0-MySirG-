8d9a17592be50d03711eab3ef1a6d1df620e3748
manoj <manojadabala123@gmail.com>
2025-02-19 21:42:36 +0530

    change i varaible to si

--- assignment 2/ques3.c
+++ assignment 2/ques3.c
@@ -1,10 +1,10 @@
 //calculate simple intrest
 #include<stdio.h>
 int main(){
-    float i,p,r,t;
+    float si,p,r,t;
     printf("Enter principal amount,rate of intrest and time period:");
     scanf("%f%f%f",&p,&r,&t);
-    i=(p*r*t)/100;
-    printf("Interest is %.1f and total amount after interest is %.1f",i,p+i);
+    si=(p*r*t)/100;
+    printf("Interest is %.1f and total amount after interest is %.1f",si,p+si);
     return 0;
 }
