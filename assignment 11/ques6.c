a3a064c43c54da54a2e4378dabc615cbc158875a
manoj <manojadabala123@gmail.com>
2025-02-25 12:44:33 +0530

    add 6th solution for assignment 11

---
+++ assignment 11/ques6.c
@@ -0,0 +1,10 @@
+//print first 10 even natural numbers.
+#include<stdio.h>
+int main(){
+    int iter=2;
+    while(iter<=20){
+        printf("%d\n",iter);
+        iter = iter + 2;
+    }
+    return 0;
+}
