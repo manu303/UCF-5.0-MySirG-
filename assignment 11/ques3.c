78f94df35731e99e126921e1ed1c0b76364e0cbe
manoj <manojadabala123@gmail.com>
2025-02-25 12:30:35 +0530

    add 2rd solution for assignment 11

---
+++ assignment 11/ques3.c
@@ -0,0 +1,10 @@
+//print first 10 natural numbers in reverse order.
+#include<stdio.h>
+int main(){
+    int iter=10;
+    while(iter>=1){
+        printf("%d\n",iter);
+        iter--;
+    }
+    return 0;
+}
