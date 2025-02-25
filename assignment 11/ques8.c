d0b05630ace77ea4c6a77ba36f0defc244b9608d
manoj <manojadabala123@gmail.com>
2025-02-25 12:49:47 +0530

    add 8th solution for assignment 11

---
+++ assignment 11/ques8.c
@@ -0,0 +1,10 @@
+//print squares of first 10 natural numbers.
+#include<stdio.h>
+int main(){
+    int iter=1;
+    while(iter<=10){
+        printf("%d\n",iter*iter);
+        iter++;
+    }
+    return 0;
+}
