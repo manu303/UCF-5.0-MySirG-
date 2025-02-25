2e4428fb8d782f266afc08856c0be7cdc8fa2d03
manoj <manojadabala123@gmail.com>
2025-02-25 12:37:47 +0530

    add 4th solution for assignment 11

---
+++ assignment 11/ques4.c
@@ -0,0 +1,10 @@
+//print first ten odd natural numbers.
+#include<stdio.h>
+int main(){
+    int iter=1;
+    while(iter<=20){
+        printf("%d\n",iter);
+        iter = iter +2;
+    }
+    return 0;
+}
