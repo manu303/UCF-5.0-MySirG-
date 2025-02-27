cd76037aa37ae6383fcf2516c675eebae385ece9
manoj <manojadabala123@gmail.com>
2025-02-27 13:56:51 +0530

    add 4th solution for assignment 13

---
+++ assignment 13/ques4.c
@@ -0,0 +1,15 @@
+//program to calculate sum of sqaures of first N natural numbers.
+#include<stdio.h>
+int main(){
+    int iter=1,sum=0,N;
+    printf("Enter the N: ");
+    scanf("%d",&N);
+    while (iter<=N)
+    {
+        sum = sum+(iter*iter);
+        iter++;
+    }
+    printf("Sum is %d",sum);
+    return 0;
+    
+}
