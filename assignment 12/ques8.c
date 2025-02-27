6ad1abf57853ee4c6d0b48da8b75c71111b65d3d
manoj <manojadabala123@gmail.com>
2025-02-27 13:14:39 +0530

    add 8th solution for assignment 12

---
+++ assignment 12/ques8.c
@@ -0,0 +1,13 @@
+//program to print squares of the first n natural numbers.
+#include<stdio.h>
+int main(){
+    int iter=1,N;
+    printf("Enter the N: ");
+    scanf("%d",&N);
+    while (iter<=N)
+    {
+        printf("%d\n",iter*iter);
+        iter++;
+    }
+    return 0; 
+}
