1a87e68779195a51598dd25445086d925d73f5cf
manoj <manojadabala123@gmail.com>
2025-02-27 13:05:13 +0530

    add 7th solution for assignment 12

---
+++ assignment 12/ques7.c
@@ -0,0 +1,25 @@
+//program to print first N natural even numbers in reverse order.
+#include<stdio.h>
+int main(){
+    int N;
+    printf("Enter the N: ");
+    scanf("%d",&N);
+    if (N%2!=0)
+    {
+        N=N-1;
+        while (N>=2)
+        {
+            printf("%d\n",N);
+            N = N-2;
+        } 
+    }
+    else{
+        while (N>=2)
+        {
+            printf("%d\n",N);
+            N = N-2;
+        } 
+    }
+    return 0;
+    
+}
