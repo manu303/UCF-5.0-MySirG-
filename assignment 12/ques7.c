38fd7d7e75df4d18edcf82d0fdcc6ded1333cc96
manoj <manojadabala123@gmail.com>
2025-02-28 11:32:44 +0530

    modify solution for ques7

--- assignment 12/ques7.c
+++ assignment 12/ques7.c
@@ -7,19 +7,12 @@ int main(){
     if (N%2!=0)
     {
         N=N-1;
-        while (N>=2)
-        {
-            printf("%d\n",N);
-            N = N-2;
-        } 
     }
-    else{
-        while (N>=2)
-        {
-            printf("%d\n",N);
-            N = N-2;
-        } 
-    }
+    while (N>=2)
+    {
+        printf("%d\n",N);
+        N = N-2;
+    } 
     return 0;
     
 }
