673a399acb5085e546f903253a815c0aae2bb6ff
manoj <manojadabala123@gmail.com>
2025-03-04 21:19:44 +0530

    add last solution for assignment 14

---
+++ assignment 14/ques5.c
@@ -0,0 +1,15 @@
+//program to print given number in reverse.
+#include<stdio.h>
+int main(){
+
+    int num,result=0;
+    printf("Enter any +ve number: ");
+    scanf("%d",&num);
+    while ((num%10)>0)
+    {
+        result=(result*10) + (num%10);
+        num=num/10;
+    }
+    printf("The reversed number is :%d",result);
+    return 0;
+}
