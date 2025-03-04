831c8d0b4d42bce1fc2003ec389416b20c2c88dd
manoj <manojadabala123@gmail.com>
2025-03-04 12:13:31 +0530

    add 3rd solution for assignment 14

---
+++ assignment 14/ques3.c
@@ -0,0 +1,21 @@
+//check given number is prime or not.
+#include<stdio.h>
+int main(){
+    int num,count=0,i=2;
+    printf("Enter any number greater than 1: ");
+    scanf("%d",&num);
+    while(i<num){
+        if(num%i==0){
+            count++;
+        }
+        i++;
+    }
+
+    if (count>0)
+        printf("Not a prime number.");
+    else
+        printf("Prime number.");
+
+    return 0;
+    
+}
