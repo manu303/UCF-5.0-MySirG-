cccdcdca3e5191d39f84f93b7fbc4d8af4ffc13e
manoj <manojadabala123@gmail.com>
2025-03-04 12:36:49 +0530

    modify solution for  ques3

--- assignment 14/ques3.c
+++ assignment 14/ques3.c
@@ -4,7 +4,7 @@ int main(){
     int num,count=0,i=2;
     printf("Enter any number greater than 1: ");
     scanf("%d",&num);
-    while(i<num){
+    while(i<=num/2){
         if(num%i==0){
             count++;
         }
