83692d38662e976af85cec69866eae51bd8e8afa
manoj <manojadabala123@gmail.com>
2025-03-05 16:14:01 +0530

    add 4th solution for assignemnt 15

---
+++ assignment 15/ques4.c
@@ -0,0 +1,27 @@
+//program to calculate HCF of two numbers.
+#include<stdio.h>
+int main(){
+    int num1,num2,diff,i;
+    printf("Enter any two +ve number: ");
+    scanf("%d%d",&num1,&num2);
+    if(num1>num2)
+        diff=num1-num2;
+    else
+        diff=num2-num1;
+    if(diff==0)
+        printf("%d",num1); //or num2
+    if((num1%diff==0)&&(num2%diff==0))
+        printf("HCF is %d",diff);
+    else{
+        i=diff/2;
+        while (i>0)
+        {
+            if((num1%i==0)&&(num2%i==0)){
+                printf("HCF is %d",i);
+                break;
+            }
+            i--;
+        } 
+    }
+    return 0;
+}
