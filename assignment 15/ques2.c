9855a452aa68f8571b2fe0fd00dec030fadf10a4
manoj <manojadabala123@gmail.com>
2025-03-05 13:24:41 +0530

    add 2nd solution for assignment 15

--- assignment 15/ques2.c
+++ assignment 15/ques2.c
@@ -2,5 +2,24 @@
 #include<stdio.h>
 int main(){
     int i,num;
-    printf
+    printf("Enter starting and ending number: ");
+    scanf("%d%d",&i,&num);
+    while(i<num){
+        int count=0,j=2;
+        while (j<=i/2)
+        {
+            if (i%j==0)
+            {
+                count++;
+                break;
+            }
+            j++;  
+        }
+        if (count==0)
+        {
+            printf("%d ",i);
+        }
+        i++; 
+    }
+    return 0;
 }
