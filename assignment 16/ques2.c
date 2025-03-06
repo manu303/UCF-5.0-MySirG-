773a06c33d09f911bfdf4c7245ea906ea3e924ee
manoj <manojadabala123@gmail.com>
2025-03-06 16:57:00 +0530

    modify 2nd solution

--- assignment 16/ques2.c
+++ assignment 16/ques2.c
@@ -4,15 +4,26 @@ int main(){
     int a=0,b=1,c,term,i=1;
     printf("Enter the term that you want: ");
     scanf("%d",&term);
-    printf("%d %d ",a,b);
-    while (i<=(term-2))
-    {
+    if(term==0){
+        printf("[]");
+    }
+    else if(term==1){
+        printf("%d ",0);
+    }
+    else if(term==2){
+        printf("%d %d ",0,1);
+    }
+    else{
+        printf("%d %d ",a,b);
+        while (i<=(term-2)){
         c=a+b;
         a=b;
         b=c;
         printf("%d ",c);
         i++;
+        }
     }
+    
     return 0;
     
 }
