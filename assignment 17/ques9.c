06f9859b79dabe933e1c70ecb5b692ceac0ce94e
manoj <manojadabala123@gmail.com>
2025-03-11 07:23:50 +0530

    add 9th solution for assignment 17

---
+++ assignment 17/ques9.c
@@ -0,0 +1,21 @@
+/*
+ABCDE
+ BCDE
+  CDE
+   DE
+    E
+*/
+#include<stdio.h>
+int main(){
+    int i,j;
+    for(i=1;i<=5;i++){
+        for(j=1;j<=5;j++){
+            if(j>=i)
+                printf("%c ",(65+j-1));
+            else
+                printf("  ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
