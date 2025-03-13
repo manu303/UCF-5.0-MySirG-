3ac14d20c3337ea0d8b5c75f6b0b188dacb1b213
manoj <manojadabala123@gmail.com>
2025-03-13 15:51:19 +0530

    add 1st solution for assignment 20

---
+++ assignment 20/ques1.c
@@ -0,0 +1,37 @@
+//display grade obtained by student in test
+#include<stdio.h>
+int main(){
+    int marks;
+    printf("Enter your marks: ");
+    scanf("%d",&marks);
+    switch (marks)
+    {
+    case 90 ... 100:
+        printf("Grade A\n");
+        break;
+
+    case 80 ... 89:
+        printf("Grade B\n");
+        break;
+
+    case 70 ... 79:
+        printf("Grade C\n");
+        break;
+
+    case 60 ... 69:
+        printf("Grade D\n");
+        break;
+
+    case 50 ... 59:
+        printf("Grade E\n");
+        break;
+
+    case 1 ... 49:
+        printf("Grade F\n");
+        break;
+    
+    default:
+        printf("Invalid marks\n");
+    }
+    return 0;
+}
