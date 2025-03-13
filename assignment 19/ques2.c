582f42ca126f331228ef5433a488d90b1fdbb2a9
manoj <manojadabala123@gmail.com>
2025-03-13 07:39:01 +0530

    add 2nd solution for assignment 19

---
+++ assignment 19/ques2.c
@@ -0,0 +1,39 @@
+//menu driven program -->add,subtract,mul,div,exit
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    while(1)
+    {
+        int choice;
+        float a,b;
+        printf("Choose one option:\n");
+        printf("1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n5.Exit\n\n");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+            case 1:
+                printf("\nEnter Two numbers: ");
+                scanf("%f%f",&a,&b);
+                printf("Sum is %.1f\n\n",a+b);
+                break;
+            case 2:
+                printf("\nEnter Two numbers: ");
+                scanf("%f%f",&a,&b);
+                printf("Difference is %.1f\n\n",a-b);
+                break;
+            case 3:
+                printf("\nEnter Two numbers: ");
+                scanf("%f%f",&a,&b);
+                printf("Product is %.1f\n\n",a*b);
+                break;
+            case 4:
+                printf("\nEnter Two numbers: ");
+                scanf("%f%f",&a,&b);
+                printf("Division is %.1f\n\n",a/b);
+                break;
+            case 5:
+                exit(0);
+    }
+}
+    return 0;
+}
