14edc8fa4aae6762235d0e0801ff8907a292895b
manoj <manojadabala123@gmail.com>
2025-03-20 15:28:13 +0530

    add 1st solution for assignment 23

---
+++ assignment 23/ques1.c
@@ -0,0 +1,23 @@
+//function to calculate LCM of two numbers.(TSRS)
+#include<stdio.h>
+int LCM(int num1,int num2);
+int main()
+{
+    int num1,num2,result;
+    printf("Enter two numbers: ");
+    scanf("%d%d",&num1,&num2);
+    result = LCM(num1,num2);
+    printf("The LCM of given numbers %d,%d is %d",num1,num2,result);
+    return 0;
+}
+
+int LCM(int num1,int num2)
+{
+    int i;
+    i = num1<num2?num1:num2;
+    for(i;i<=num1*num2;i++){
+        if((i%num1==0) && (i%num2==0))
+            return i;
+    }
+}
+
