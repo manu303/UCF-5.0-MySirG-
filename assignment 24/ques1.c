77a0fb31c717b083e76eaaa7e00fbc50e307b6c6
manoj <manojadabala123@gmail.com>
2025-03-20 17:07:14 +0530

    add 1st solution for assignment 24

---
+++ assignment 24/ques1.c
@@ -0,0 +1,26 @@
+//function to find the prime numbers between two numbers. (TSRN)
+#include<stdio.h>
+void nprimes(int start,int end);
+int main()
+{
+    int start,end,result;
+    printf("Enter starting and ending number: ");
+    scanf("%d%d",&start,&end);
+    nprimes(start,end);
+    return 0;
+}
+
+void nprimes(int start,int end)
+{
+    int i,j;
+    for(i=start;i<=end;i++){
+        for(j=2;j<i;j++)
+        {
+            if(i%j==0)
+                break;
+        }
+        if(i==j)
+            printf("%d ",i);
+    }
+    
+}
