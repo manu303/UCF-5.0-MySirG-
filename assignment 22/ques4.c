c574fa758eed064992e7cd8a7912325b916f4c10
manoj <manojadabala123@gmail.com>
2025-03-20 11:25:36 +0530

    add 4th solution for assignment 22

---
+++ assignment 22/ques4.c
@@ -0,0 +1,25 @@
+//function to check whether a given number contains a given digit or not.(TSRS)
+#include<stdio.h>
+int check(int num,int digit);
+int main()
+{
+    int num,digit,result;
+    printf("Enter number and digit: ");
+    scanf("%d%d",&num,&digit);
+    result = check(num,digit);
+    if(result)
+        printf("The digit %d is present in %d",digit,num);
+    else
+        printf("The digit %d is not present in %d",digit,num);
+    return 0;
+}
+
+int check(int num,int digit)
+{
+    for(num;num;num=num/10)
+    {
+        if((num%10)==digit)
+            return 1;
+    }
+    return 0;
+}
