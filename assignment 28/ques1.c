bb276d33b2b3e7fa07b0794f7d78de0cf4727b76
manoj <manojadabala123@gmail.com>
2025-03-25 21:32:34 +0530

    add 1st solution for assignment 28

---
+++ assignment 28/ques1.c
@@ -0,0 +1,19 @@
+//recursive function to print sum of factorial of given number.
+#include<stdio.h>
+int fact(int n);
+int main()
+{
+    int n,res;
+    printf("Enter n: ");
+    scanf("%d",&n);
+    res = fact(n);
+    printf("The factorial is %d",res);
+    return 0;
+}
+
+int fact(int n)
+{
+    if(n==0)
+        return 1;
+    return n * fact(n-1);
+}
