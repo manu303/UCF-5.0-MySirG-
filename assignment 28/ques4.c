05c77cfabe8a2b6b847488954822aefd38745f66
manoj <manojadabala123@gmail.com>
2025-03-26 10:44:10 +0530

    add 4th solution for assignment 28

---
+++ assignment 28/ques4.c
@@ -0,0 +1,20 @@
+//count the number of digits of a given number using recursion
+#include<stdio.h>
+int countdigits(int n,int count);
+int main()
+{
+    int n,res,count=1;
+    printf("Enter num: ");
+    scanf("%d",&n);
+    res=countdigits(n,count);
+    printf("no of digits are %d ",res);
+    return 0;
+}
+
+int countdigits(int n,int count)
+{
+    
+    if(n<10)
+        return count;
+    countdigits(n/10,count+1);
+}
