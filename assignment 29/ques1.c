d2b0a75ca69c83b948016f05890b4cc9c062af67
manoj <manojadabala123@gmail.com>
2025-03-27 15:28:19 +0530

    add 1st solution for assignment 29

---
+++ assignment 29/ques1.c
@@ -0,0 +1,13 @@
+//
+#include<stdio.h>
+int main()
+{
+    int a[10],i,sum=0;
+    printf("Enter 10 numbers: ");
+    for(i=0;i<=9;i++)
+        scanf("%d",&a[i]);
+    for(i=0;i<=9;i++)
+        sum=sum+a[i];
+    printf("The sum is %d",sum);
+    return 0;
+}
