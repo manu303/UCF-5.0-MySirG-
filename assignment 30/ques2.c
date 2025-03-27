d5e2aa1d0d3b12347aa3dd8e1d8b8a123c3dddbc
manoj <manojadabala123@gmail.com>
2025-03-27 21:16:29 +0530

    add 2nd solution for assignment 30

---
+++ assignment 30/ques2.c
@@ -0,0 +1,54 @@
+//print second largest element in array.
+#include<stdio.h>
+int input(int b[],int n);
+int sort(int c[],int n);
+int secondlargest(int d[],int n);
+int main()
+{
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d",&n);
+    int a[n];
+    printf("Enter %d number:",n);
+    input(a,n);
+    sort(a,n);
+    printf("Second largest element is:");
+    secondlargest(a,n);
+    return 0;
+}
+
+int input(int b[],int n)
+{
+    int i;
+    for(i=0;i<n;i++)
+        scanf("%d",&b[i]); 
+}
+
+int sort(int c[],int n)
+{
+    int i,j;
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(c[i] > c[j])
+            {
+                c[i] = c[i];
+                c[j] = c[j];
+            }
+            else
+            {
+                c[i] = c[i] + c[j];
+                c[j] = c[i] - c[j];
+                c[i] = c[i] - c[j];
+                
+                // (or) c[i] = c[i] + c[j] - (c[j]=c[i]);
+            }
+        }
+    }
+}
+
+int secondlargest(int d[],int n)
+{
+        printf("%d ",d[1]);
+}
