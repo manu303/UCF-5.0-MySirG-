9e1a2455e615a8eb27eb1fa6dae0d91e51fb1071
manoj <manojadabala123@gmail.com>
2025-03-28 07:17:16 +0530

    add 2nd solution for assignment 31

---
+++ assignment 31/ques2.c
@@ -0,0 +1,31 @@
+//function to find smallest number in an array
+#include<stdio.h>
+int input(int b[],int n);
+int smallestnum(int c[],int n);
+int main()
+{
+    int n,res;
+    printf("enter size of an array: ");
+    scanf("%d",&n);
+    int a[n];
+    printf("enter %d numbers: ",n);
+    input(a,n);
+    res = smallestnum(a,n);
+    printf("The smallest element in a given array is %d",res);
+}
+
+int input(int b[],int n)
+{
+    int i;
+    for(i=0;i<n;i++)
+        scanf("%d",&b[i]);
+}
+
+int smallestnum(int c[],int n)
+{
+    int i,small;
+    small = c[0]<c[1]?c[0]:c[1];
+    for(i=0;i<n;i++)
+        small = small < c[i]?small:c[i];
+    return small;
+}
