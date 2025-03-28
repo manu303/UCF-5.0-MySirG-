db9f2f632b2ab56535e8505239c025ec5f662adc
manoj <manojadabala123@gmail.com>
2025-03-28 07:15:43 +0530

    modify 1st solution

--- assignment 31/ques1.c
+++ assignment 31/ques1.c
@@ -11,7 +11,7 @@ int main()
     printf("Enter %d numbers: ",n);
     input(a,n);
     res = greatestnum(a,n);
-    printf("The greatst number in given array is %d",res);
+    printf("The greatest number in given array is %d",res);
     return 0;
 }
 
@@ -24,11 +24,11 @@ int input(int c[],int n)
 
 int greatestnum(int b[],int n)
 {
-    int i,j,big;
+    int i,big;
     big = b[0]>b[1]?b[0]:b[1];
     for(i=0;i<n;i++)
     {
-        big = big>b[i+1]?big:b[i+1];
+        big = big>b[i]?big:b[i];
     }
     return big;
 }
