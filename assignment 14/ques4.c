2ee22920c357ccd877b8dd002a81b27590dbb301
manoj <manojadabala123@gmail.com>
2025-03-29 16:56:18 +0530

    modify 4th solution

--- assignment 14/ques4.c
+++ assignment 14/ques4.c
@@ -8,10 +8,8 @@ int main(){
     for(i=max;i<=num1*num2;i++)
     {
         if(i%num1==0 && i%num2==0)
-        {
-            printf("%d",i);
-            break;
-        }     
+            break;    
     }
+    printf("LCM is %d",i);
     return 0;
 } 
