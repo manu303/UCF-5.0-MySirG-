438dba954f3aaa60046107fa628a0fc9c7278773
manoj <manojadabala123@gmail.com>
2025-03-29 17:17:23 +0530

    modify 2nd solution

--- assignment 15/ques2.c
+++ assignment 15/ques2.c
@@ -1,25 +1,18 @@
 //program to print prime numbers between given numbers.
 #include<stdio.h>
 int main(){
-    int i,num;
+    int num1,num2,i,j;
     printf("Enter starting and ending number: ");
-    scanf("%d%d",&i,&num);
-    while(i<num){
-        int count=0,j=2;
-        while (j<=i/2)
+    scanf("%d%d",&num1,&num2);
+    for(i=num1+1;i<num2;i++)
+    {
+        for(j=2;j<=i;j++)
         {
-            if (i%j==0)
-            {
-                count++;
+            if(i%j==0)
                 break;
-            }
-            j++;  
         }
-        if (count==0)
-        {
+        if(i==j)
             printf("%d ",i);
-        }
-        i++; 
     }
     return 0;
 }
