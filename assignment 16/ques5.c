58d54e3b3a295158fdd26858b51cf79192221505
manoj <manojadabala123@gmail.com>
2025-03-29 22:16:34 +0530

    modify 5th solution

--- assignment 16/ques5.c
+++ assignment 16/ques5.c
@@ -1,28 +1,35 @@
 //program to print all armstrong numbers under 1000
-#include<stdio.h>
-int main(){
-    int i=0;
-    while(i<1000){
-        int a=i,b=i,count=1,sum=0;
-        while(a/10>0){
+#include <stdio.h>
+
+int main()
+{
+    int j;
+    for(j=0;j<1000;j++)
+    {
+        int count=0,x,y;
+        x=j;
+        y=j;
+        //count digits
+        while(x)
+        {
+            x=x/10;
             count++;
-            a=a/10;
         }
-        int j=1;
-        while(j<=count){
-            int digits=count,prod=1;
-            while (digits>0)
+        int digits,i,sum=0,prod;
+        for(i=1;i<=count;i++)
+        {
+            digits = count;
+            prod=1;
+            while(digits)
             {
-                prod= prod*(b%10);
-                digits--;
+            prod = prod * (y%10);
+            digits--;
             }
-            sum = sum+prod;
-            b=b/10;
-            j++;
+            sum = sum + prod;
+            y=y/10;
         }
-        if(sum==i)
-            printf("%d ",i);
-        i++;
+        if(sum == j)
+            printf("%d ",j);
     }
     return 0;
 }
