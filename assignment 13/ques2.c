75481a4a40d341074739b08328075300de8d5dca
manoj <manojadabala123@gmail.com>
2025-03-29 12:03:39 +0530

    modify 2nd solution

--- assignment 13/ques2.c
+++ assignment 13/ques2.c
@@ -1,13 +1,13 @@
-//program to calculate sum of N natural numbers.
+//program to calculate sum of N even natural numbers.
 #include<stdio.h>
 int main(){
-    int iter=2,sum=0,N;
+    int iter=1,sum=0,N;
     printf("Enter the N: ");
     scanf("%d",&N);
     while (iter<=N)
     {
-        sum=sum+iter;
-        iter=iter+2;
+        sum=sum+2*iter;
+        iter++;
     }
     printf("Sum is %d",sum);
     return 0;
