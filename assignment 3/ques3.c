59cce5d547486a7f18bd04ff5a599f4294a30aa1
manoj <manojadabala123@gmail.com>
2025-03-29 07:18:42 +0530

    modify 3rd solution

--- assignment 3/ques3.c
+++ assignment 3/ques3.c
@@ -2,14 +2,10 @@
 #include<stdio.h>
 int main(){
     
-    char character1,character2,character3 ;
-    int result1,result2,result3;
-    printf("Enter any 3 charcaters consecutively:");
-    scanf("%c%c%c",&character1,&character2,&character3);
-    result1=character1;
-    result2=character2;
-    result3=character3;
-    printf("The ASCII Codes are:%d,%d,%d",result1,result2,result3);
+    char character1,character2,character3;
+    printf("Enter any 3 charcaters:");
+    scanf("%c %c %c",&character1,&character2,&character3);
+    printf("The ASCII Codes are:%d,%d,%d",character1,character2,character3);
     return 0;
 
 }
