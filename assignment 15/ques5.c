6a6cd47779342ae86fcda00d81199adf450f5817
manoj <manojadabala123@gmail.com>
2025-03-29 17:34:31 +0530

    modify 5th solution

--- assignment 15/ques5.c
+++ assignment 15/ques5.c
@@ -1,39 +1,20 @@
 //program to check whether two given numbers are co-prime numbers or not.
 #include<stdio.h>
 int main(){
-    int num1,num2,diff,i;
-    printf("Enter any two numbers: ");
+    int num1,num2,min,i;
+    printf("Enter any two +ve number: ");
     scanf("%d%d",&num1,&num2);
-    if(num1>num2)
-        diff = num1-num2;
-    else
-        diff = num2-num1;
-    if(diff==0)
-        printf("Given numbers %d and %d are not co-primes.",num1,num2);
-    else if((num1%diff==0)&&(num2%diff==0)){
-            if(diff==1)
-                printf("Given numbers %d and %d are co-primes.",num1,num2);
-            else
-                printf("Given numbers %d and %d are not co-primes.",num1,num2);
-    }
-    else{
-        i=diff/2;
-        while ((i>0))
+    min = num1<num2?num1:num2;
+    for(i=min;i>0;i--)
+    {
+        if(num1%i==0 && num2%i==0)
         {
-            if ((num1%i==0)&&(num2%i==0))
-            {
-                if ((i==1))
-                {
-                    printf("Given numbers %d and %d are co-primes.",num1,num2);
-                    break;  
-                }
-                else{
-                    printf("Given numbers %d and %d are not co-primes.",num1,num2);
-                    break;
-                }    
-            }
-            i--;  
-        }   
+            break;
+        }
     }
+    if(i==1)
+        printf("%d and %d are Co primes numbers.",num1,num2);
+    else
+        printf("%d and %d are not Co primes",num1,num2);
     return 0;
 }
