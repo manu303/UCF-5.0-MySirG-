8a0abe3b02f4d37f9b8f460f23fa10d93fef70ca
manoj <manojadabala123@gmail.com>
2025-03-29 13:23:43 +0530

    modify 5th solution

--- assignment 14/ques5.c
+++ assignment 14/ques5.c
@@ -5,7 +5,7 @@ int main(){
     int num,result=0;
     printf("Enter any +ve number: ");
     scanf("%d",&num);
-    while ((num%10)>0)
+    while (num)
     {
         result=(result*10) + (num%10);
         num=num/10;
