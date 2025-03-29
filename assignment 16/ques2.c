72e5437cad9890825f4bbb7f5ca1405ddc30093a
manoj <manojadabala123@gmail.com>
2025-03-29 21:48:10 +0530

    modify 2nd solution

--- assignment 16/ques2.c
+++ assignment 16/ques2.c
@@ -1,29 +1,17 @@
 //program to print first n terms of fibonacci numbers.
 #include<stdio.h>
 int main(){
-    int a=0,b=1,c,term,i=1;
-    printf("Enter the term that you want: ");
+    int term,a=-1,b=1,c;
+    printf("Enter term: ");
     scanf("%d",&term);
-    if(term==0){
-        printf("[]");
-    }
-    else if(term==1){
-        printf("%d ",0);
-    }
-    else if(term==2){
-        printf("%d %d ",0,1);
-    }
-    else{
-        printf("%d %d ",a,b);
-        while (i<=(term-2)){
+    while(term)
+    {
         c=a+b;
+        printf("%d ",c);
         a=b;
         b=c;
-        printf("%d ",c);
-        i++;
-        }
+        term--;
     }
     
     return 0;
-    
 }
