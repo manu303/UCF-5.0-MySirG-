9d959340113600efd0290b12c1e84ca6789705f1
manoj <manojadabala123@gmail.com>
2025-03-31 14:22:50 +0530

    modify 1st solution

--- assignment 23/ques1.c
+++ assignment 23/ques1.c
@@ -14,7 +14,7 @@ int main()
 int LCM(int num1,int num2)
 {
     int i;
-    i = num1<num2?num1:num2;
+    i = num1>num2?num1:num2;
     for(i;i<=num1*num2;i++){
         if((i%num1==0) && (i%num2==0))
             return i;
