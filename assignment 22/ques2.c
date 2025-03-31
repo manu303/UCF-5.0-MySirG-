7aa44778eec54438b95d2e3848cc7cc43ebd526c
manoj <manojadabala123@gmail.com>
2025-03-31 12:48:49 +0530

    modify 2nd solution

--- assignment 22/ques2.c
+++ assignment 22/ques2.c
@@ -1,6 +1,7 @@
 //calculate the no of combinations one can make from n items and r selected at a time(TSRS)
 #include<stdio.h>
 int comb(int n,int r);
+int fact(int n);
 int main()
 {
     int n,r,result;
@@ -10,23 +11,14 @@ int main()
     printf("The no of combinations are %d",result);
     return 0;
 }
-
 int comb(int n,int r)
 {
-    int prod1=1,prod2=1,prod3=1;
-    if(n>=r)
-    {
-        //for n
-        for(int i=1;i<=n;i++)
-            prod1=prod1*i;
-        //for r
-        for(int j=1;j<=r;j++)
-            prod2=prod2*j;
-        //for n-r
-        for(int k=1;k<=(n-r);k++)
-            prod3=prod3*k;
-        return (prod1)/(prod2*prod3);
-    }
-    else
-        return 0;
+    return fact(n)/(fact(n-r)*fact(r));
+}
+int fact(int n)
+{
+    int fact=1,i;
+    for(i=1;i<=n;i++)
+        fact = fact * i;
+    return fact;
 }
