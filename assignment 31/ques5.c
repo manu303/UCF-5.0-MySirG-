522fe57d8e959266a638ab577e9b5535cd81d8bd
manoj <manojadabala123@gmail.com>
2025-03-31 18:50:48 +0530

    add last solution for assignment 31

---
+++ assignment 31/ques5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+void input(int a[],int n)
+{
+    int i;
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+}
+int adjduplicate(int a[],int n)
+{
+    int i;
+    for(i=0;i<(n-1);i++)
+    {
+        if(a[i+1] == a[i])
+            return a[i+1];
+    }
+    return -2;
+}
+
+int main()
+{
+    int n,res;
+    printf("Enter size of an array: ");
+    scanf("%d",&n);
+    int arr[n];
+    input(arr,n);
+    res = adjduplicate(arr,n);
+    if(res==-2)
+    {
+        printf("No adjacent duplicate value present");
+        exit(0);
+    }
+    printf("The adjacent duplicate value is %d",res);
+    return 0;
+}
