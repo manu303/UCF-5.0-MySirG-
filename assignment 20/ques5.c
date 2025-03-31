4113002894842140156c62626bf05dfb5db1aa9d
manoj <manojadabala123@gmail.com>
2025-03-31 11:07:54 +0530

    modify 5th solution

--- assignment 20/ques5.c
+++ assignment 20/ques5.c
@@ -47,7 +47,7 @@ int main(){
             printf("Enter +ve number:");
             scanf("%d",&n);
             for(i=2;i<n;i++){
-                if(n%2==0){
+                if(n%i==0){
                 break;
              }
             }
