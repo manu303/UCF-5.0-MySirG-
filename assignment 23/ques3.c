f062f6fb6a922bd421aa5e6ed2d9acaa4f37f3c8
manoj <manojadabala123@gmail.com>
2025-03-31 14:49:16 +0530

    modify 3rd solution

--- assignment 23/ques3.c
+++ assignment 23/ques3.c
@@ -17,12 +17,9 @@ int main()
 int prime(int num)
 {
     int i;
-    for(i=2;i<=num;i++){
+    for(i=2;i<num;i++){
         if(num%i==0)
-            break;
+            return 0;
     }
-    if(i==num)
-        return 1;
-    else
-        return 0;
+    return 1;
 }
