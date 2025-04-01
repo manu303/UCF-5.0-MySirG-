3af29967ef8cdc04a07c23a3e5ff20fd8dfa867a
manoj <manojadabala123@gmail.com>
2025-04-01 15:58:29 +0530

    modify 5th solution

--- assignment 25/ques5.c
+++ assignment 25/ques5.c
@@ -1,19 +1,20 @@
 //recursive function to print even natural numbers.
-#include<stdio.h>
-int evennatnum(int n);
-int main()
+// Online C compiler to run C program online
+#include <stdio.h>
+
+void FirstNnaturalNumseven(int n)
 {
+    if(n>0)
+    {
+        FirstNnaturalNumseven(n-1);
+        printf("%d ",2*n);
+    }
+}
+
+int main() {
     int n;
     printf("Enter n: ");
     scanf("%d",&n);
-    evennatnum(n);
+    FirstNnaturalNumseven(n);
     return 0;
-}
-
-int evennatnum(int n)
-{
-    if(n==1)
-        return printf("%d ",2);
-    evennatnum(n-1);
-    printf("%d ",2*n);
 }
