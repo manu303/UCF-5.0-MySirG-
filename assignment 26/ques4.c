bdf9e1ff8a0dd55be5bbd7b3a232f1a96debf61f
manoj <manojadabala123@gmail.com>
2025-04-01 17:11:04 +0530

    modify 4th solution

--- assignment 26/ques4.c
+++ assignment 26/ques4.c
@@ -1,19 +1,19 @@
 //recursive function to print octal of a given decimal number
-#include<stdio.h>
-int oct(int n);
-int main()
+#include <stdio.h>
+
+void octal(int n)
 {
+    if(n>0)
+    {
+        octal(n/8);
+        printf("%d",n%8);
+    }
+}
+
+int main() {
     int n;
     printf("Enter n: ");
     scanf("%d",&n);
-    oct(n);
+    octal(n);
     return 0;
-}
-
-int oct(int n)
-{
-    if(n<8)
-        return printf("%d",n);
-    oct(n/8);
-    printf("%d",n%8);
 }
