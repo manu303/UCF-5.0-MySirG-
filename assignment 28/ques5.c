c7603e9212603341dc5523b45155b66a830c932b
manoj <manojadabala123@gmail.com>
2025-04-01 22:03:22 +0530

    modify 5th solution

--- assignment 28/ques5.c
+++ assignment 28/ques5.c
@@ -1,28 +1,21 @@
 //program to print power of any given number.
-#include<stdio.h>
-#include<stdlib.h>
-int powofnum(int base,int pow);
-int main()
-{
-    int base,pow,res;
-    printf("Enter base number and power: ");
-    scanf("%d%d",&base,&pow);
-    res = powofnum(base,pow);
-    printf("The result is %d",res);
-    return 0;
-}
+#include <stdio.h>
 
-int powofnum(int base,int pow)
+double power(double x,double y)
 {
-    if(pow==0 && base==0)
-    {
-        printf("undefined");
-        exit(0);
-    }
-    else if(pow==0)
+    if(y==0)
         return 1;
-    else if(pow==1)
-        return base;
-    else
-        return base * powofnum(base,pow-1);
+    else if(y>0)
+        return x*power(x,y-1);
+    else 
+        return 1/x*power(x,y+1);
+}   
+
+
+int main() {
+    double x,y;
+    printf("Enter base and exponent:");
+    scanf("%lf%lf",&x,&y);
+    printf("power of given number is %.1lf",power(x,y));
+    return 0;
 }
