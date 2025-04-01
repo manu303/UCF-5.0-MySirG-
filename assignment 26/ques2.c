ca6aeeb70e9d1170341e239831bc1102f8a7c3da
manoj <manojadabala123@gmail.com>
2025-04-01 16:10:16 +0530

    modify 2nd solution

--- assignment 26/ques2.c
+++ assignment 26/ques2.c
@@ -1,19 +1,19 @@
 //recursive function to print squares of first n natural numbers.
-#include<stdio.h>
-int squares(int n);
-int main()
+#include <stdio.h>
+
+void FirstNnaturalNumsSquares(int n)
 {
+    if(n>0)
+    {
+        FirstNnaturalNumsSquares(n-1);
+        printf("%d ",n*n);
+    }
+}
+
+int main() {
     int n;
     printf("Enter n: ");
     scanf("%d",&n);
-    squares(n);
+    FirstNnaturalNumsSquares(n);
     return 0;
-}
-
-int squares(int n)
-{
-    if(n==1)
-        return printf("%d ",1);
-    squares(n-1);
-    printf("%d ",n*n);
 }
