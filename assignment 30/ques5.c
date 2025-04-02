1f1f9be4717d2f5ae64037eadef795dc0d475971
manoj <manojadabala123@gmail.com>
2025-04-02 11:28:04 +0530

    modify 5th solution

--- assignment 30/ques5.c
+++ assignment 30/ques5.c
@@ -1,42 +1,15 @@
 // copy the elements of one array into another array.
 #include <stdio.h>
-int input(int b[],int n);
-int copy(int b[],int c[],int n);
-int printarr(int d[],int n);
 int main() {
-    int n;
-    printf("Enter size of array: ");
-    scanf("%d",&n);
-    int a[n];
-    printf("Enter %d number:",n);
-    input(a,n);
-    int b[n];
-    copy(a,b,n); //original,copy,n
-    printf("original array:");
-    printarr(a,n);
-    printf("copied   array:");
-    printarr(b,n);
+    int a[10],b[10],i;
+    printf("Enter 10 number:");
+    for(i=0;i<10;i++)
+        scanf("%d",&a[i]);   
+    for(i=0;i<10;i++)
+        b[i] = a[i];
+    for(i=0;i<10;i++)
+        printf("%d ",b[i]); 
     return 0;
 }
 
-int input(int b[],int n)
-{
-    int i;
-    for(i=0;i<n;i++)
-        scanf("%d",&b[i]); 
-}
 
-int copy(int b[],int c[],int n)
-{
-    int i;
-    for(i=0;i<n;i++)
-        c[i] = b[i];
-}
-
-int printarr(int d[],int n)
-{
-    int i;
-    for(i=0;i<n;i++)
-        printf("%d ",d[i]);
-    printf("\n");
-}
