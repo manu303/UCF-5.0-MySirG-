2efd55af83498e26db9f96e131cda1869f7a61a6
manoj <manojadabala123@gmail.com>
2025-04-02 07:48:41 +0530

    modify 4th solution

--- assignment 29/ques4.c
+++ assignment 29/ques4.c
@@ -1,33 +1,18 @@
 //find greatest element in array
-#include<stdio.h>
-int input(int b[],int n);
-int greatest(int c[],int n);
-int main()
-{
-    int n,res;
-    printf("Enter size of array: ");
-    scanf("%d",&n);
-    int arr[n];
-    input(arr,n);
-    res = greatest(arr,n);
-    printf("The greatest number of array is %d",res);
-}
+#include <stdio.h>
 
-int input(int b[],int n)
-{
-    int i;
-    for(i=0;i<n;i++)
+int main() {
+   
+    int arr[10],i,big;
+    printf("Enter 10 numbers:");
+    for(i=0;i<10;i++)
+        scanf("%d",&arr[i]);
+    big = arr[0];
+    for(i=1;i<10;i++)
     {
-        printf("Enter %d number:",i+1);
-        scanf("%d",&b[i]);
-    }     
-}
-
-int greatest(int c[],int n)
-{
-    int i,big;
-    big = c[0]>c[1]?c[0]:c[1];
-    for(i=0;i<n;i++)
-        big = big>c[i]?big:c[i];
-    return big;
+        if(arr[i]>big)
+            big = arr[i];
+    }
+    printf("Greatest number stored in array is %d\n",big);
+    return 0;
 }
