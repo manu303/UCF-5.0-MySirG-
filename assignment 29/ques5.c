554c47f0ab205cc3ca7061d9884371263f226043
manoj <manojadabala123@gmail.com>
2025-04-02 07:49:46 +0530

    modify 5th solution

--- assignment 29/ques5.c
+++ assignment 29/ques5.c
@@ -1,33 +1,18 @@
 //find smallest element in array
-#include<stdio.h>
-int input(int b[],int n);
-int smallest(int c[],int n);
-int main()
-{
-    int n,res;
-    printf("Enter size of array: ");
-    scanf("%d",&n);
-    int arr[n];
-    input(arr,n);
-    res = smallest(arr,n);
-    printf("The smallest number of array is %d",res);
-}
+#include <stdio.h>
 
-int input(int b[],int n)
-{
-    int i;
-    for(i=0;i<n;i++)
+int main() {
+   
+    int arr[10],i,small;
+    printf("Enter 10 numbers:");
+    for(i=0;i<10;i++)
+        scanf("%d",&arr[i]);
+    small = arr[0];
+    for(i=1;i<10;i++)
     {
-        printf("Enter %d number:",i+1);
-        scanf("%d",&b[i]);
-    }     
-}
-
-int smallest(int c[],int n)
-{
-    int i,small;
-    small = c[0]<c[1]?c[0]:c[1];
-    for(i=0;i<n;i++)
-        small = small<c[i]?small:c[i];
-    return small;
+        if(arr[i]<small)
+            small = arr[i];
+    }
+    printf("Smallest number stored in array is %d\n",small);
+    return 0;
 }
