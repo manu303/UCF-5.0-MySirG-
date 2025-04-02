a9d839251f3fbf29839f1657f4741c2ab281decb
manoj <manojadabala123@gmail.com>
2025-04-02 07:47:26 +0530

    modify 3rd solution

--- assignment 29/ques3.c
+++ assignment 29/ques3.c
@@ -1,36 +1,18 @@
 //calculate sum of all even numbers and sum of all odd numbers.
-#include<stdio.h>
-int input(int a[],int n);
-int calc(int c[],int n);
-int main()
-{
-    int n,res;
-    printf("Enter size of array: ");
-    scanf("%d",&n);
-    int a[n];
-    input(a,n);
-    res = calc(a,n);
-    return 0;
-}
+#include <stdio.h>
 
-int input(int b[],int n)
-{
-    int i;
-    for(i=0;i<n;i++)
-    {
-        printf("Enter %d number: ",i+1);
-        scanf("%d",&b[i]);
-    }
-}
-
-int calc(int c[],int n)
-{
-    int even=0,odd=0,i;
-    for(i=0;i<n;i++)
-        if(c[i]%2==0)
-            even = even+c[i];
+int main() {
+   
+    int arr[10],i,even=0,odd=0;
+    printf("Enter 10 numbers:");
+    for(i=0;i<10;i++)
+        scanf("%d",&arr[i]);
+    for(i=0;i<10;i++)
+        if(arr[i]%2==0)
+            even=even+arr[i];
         else
-            odd = odd+c[i];
-    printf("The sum of even numbers is %d\n",even);
-    printf("The sum of odd numbers is %d\n",odd);
+            odd=odd+arr[i];
+    printf("sum of even numbers stored in array is %d\n",even);
+    printf("sum of odd numbers stored in array is %d\n",odd);
+    return 0;
 }
