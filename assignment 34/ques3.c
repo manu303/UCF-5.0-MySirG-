25a85912998500dc921d0a6102b3126fddea0551
manoj <manojadabala123@gmail.com>
2025-04-05 19:14:53 +0530

    modify 3rd solution

--- assignment 34/ques3.c
+++ assignment 34/ques3.c
@@ -4,12 +4,16 @@ int main()
 {
     char str[50];
     char vowels[10]={'a','e','i','o','u','A','E','I','O','U'};
+    int i,j;
     printf("Enter a string:");
     fgets(str,50,stdin);
+    for(i=0;str[i]!='\n';i++);
+    if(str[i]=='\n')
+        str[i]='\0';
     int count = 0;
-    for(int i=0;str[i];i++)
+    for(i=0;str[i];i++)
     {
-        for(int j=0;j<10;j++)
+        for(j=0;j<10;j++)
         {
             if(str[i]==vowels[j])
                 count++;
