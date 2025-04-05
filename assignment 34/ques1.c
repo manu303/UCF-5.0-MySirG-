2f5e1c67ccf452ecd9b4be744781401bbac56a86
manoj <manojadabala123@gmail.com>
2025-04-05 19:09:48 +0530

    modify 1st solution

--- assignment 34/ques1.c
+++ assignment 34/ques1.c
@@ -6,7 +6,10 @@ int main()
     char str[50];
     printf("Enter a string:");
     fgets(str,50,stdin);
+    for(i=0;str[i]!='\n';i++);
+    if(str[i]=='\n')
+        str[i]='\0';
     for(i=0;str[i];i++);
-    printf("The length of the given string is:%d",i-1);//to remove new line charcater from string (when user press enter we get new line as well along with other characters)
+    printf("The length of the given string is:%d",i);//to remove new line charcater from string (when user press enter we get new line as well along with other characters)
     return 0;
 }
