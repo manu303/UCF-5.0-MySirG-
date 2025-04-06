010ba3c56af8b0af9cfccf051fcd54b5561fd089
manoj <manojadabala123@gmail.com>
2025-04-06 10:25:20 +0530

    modify 2nd solution

--- assignment 34/ques2.c
+++ assignment 34/ques2.c
@@ -1,5 +1,25 @@
 //count the occurence of given character in a given string.
 #include<stdio.h>
+
+char lowerstr(char str[])
+{
+    int i;
+    for(i=0;str[i];i++)
+    {
+        if(str[i]>='A' && str[i]<='Z')
+        {
+            str[i] = str[i] + 32;
+        }
+    }
+}
+
+char lowerchar(char ch)
+{
+    if (ch>='A' && ch<='Z')
+        ch = ch+32;
+    return ch;
+}
+
 int main()
 {
     char ch,str[50];
@@ -9,8 +29,10 @@ int main()
     for(i=0;str[i]!='\n';i++);
     if(str[i]=='\n')
         str[i]='\0';
+    lowerstr(str);
     printf("Enter a charcter:");
     scanf("%c",&ch);
+    ch = lowerchar(ch);
     int count=0;
     for(i=0;str[i];i++)
     {
