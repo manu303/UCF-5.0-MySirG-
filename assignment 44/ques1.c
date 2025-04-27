646269b57b0fd203e900e46b182971ccd38e5b83
manu303 <manojadabala123@gmail.com>
2025-04-27 15:05:41 +0530

    add 1st solution for assignment 44

---
+++ assignment 44/ques1.c
@@ -0,0 +1,14 @@
+//define a structure Employee with member variables id,name,salary.
+#include<stdio.h>
+
+struct  Employee
+{
+    int id;
+    char name[50];
+    float salary;
+};  
+
+int main()
+{
+    return 0;
+}
