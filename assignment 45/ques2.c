91044bcab8105da3fd526185b2094f77042b288d
manu303 <manojadabala123@gmail.com>
2025-04-28 10:22:26 +0530

    add 2nd solution for assignment 45

---
+++ assignment 45/ques2.c
@@ -0,0 +1,42 @@
+//program to store information of 10 students and display them using structure.
+#include<stdio.h>
+#include<string.h>
+struct student input();
+void display(struct student stu);
+struct student
+{
+    int roll_no;
+    char name[50];
+};
+
+int main()
+{
+    struct student student[10];
+    int i;
+    for(i=0;i<10;i++)
+    {
+        student[i]=input();
+    }
+    for(i=0;i<10;i++)
+    {
+        display(student[i]);
+    }
+    return 0;
+}
+
+struct student input()
+{
+    struct student stu;
+    printf("Enter student roll number and name:");
+    scanf("%d",&stu.roll_no);
+    getchar();
+    fgets(stu.name,50,stdin);
+    return stu;
+}
+
+void display(struct student stu)
+{
+    printf("-------------------------------\n");
+    printf("Roll Number:%d\n",stu.roll_no);
+    printf("Name:%s",stu.name);
+}
