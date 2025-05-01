12b5ea9f804ddf6c72e937388dc8c0ec610bc975
manu303 <manojadabala123@gmail.com>
2025-05-01 07:52:33 +0530

    add 4th solution for assignment 2

---
+++ cpp programs/assignment 2/ques4.cpp
@@ -0,0 +1,11 @@
+//program to calculate the area of circle
+#include<iostream>
+using namespace std;
+int main()
+{
+    float radius;
+    cout<<"Enter radius:";
+    cin>>radius;
+    cout<<"Area of circle is "<<3.14*radius*radius;
+    return 0;
+}
