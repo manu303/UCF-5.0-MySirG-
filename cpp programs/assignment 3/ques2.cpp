36fc0e63616a076e7e29a06eb7be3480a8162a2e
manu303 <manojadabala123@gmail.com>
2025-05-01 08:08:33 +0530

    add 2nd solution for assignment 3

---
+++ cpp programs/assignment 3/ques2.cpp
@@ -0,0 +1,11 @@
+//program to calculate the square of a number.
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter a number:";
+    cin>>a;
+    cout<<"Square of "<<a<<" is "<<a*a;
+    return 0;
+}
