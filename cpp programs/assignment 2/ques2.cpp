2cdc1046e350f18283b3d720a180045784bcbeee
manu303 <manojadabala123@gmail.com>
2025-05-01 07:43:18 +0530

    add 2nd solution for assignment 2

---
+++ cpp programs/assignment 2/ques2.cpp
@@ -0,0 +1,8 @@
+//program to print "Hello" on the first line "MysirG" on the second line.
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello"<<endl<<"MysirG";
+    return 0;
+}
