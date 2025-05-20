42fae21b942a4e6066d3eaee93388b7f62ac7694
manu303 <manojadabala123@gmail.com>
2025-05-20 06:25:56 +0530

    modify 1st solution for assignment 13

--- cpp programs/assignment 13/ques1.cpp
+++ cpp programs/assignment 13/ques1.cpp
@@ -12,11 +12,31 @@ class fraction
         {
             cout<<numerator<<"/"<<denominator<<endl;
         }
+        fraction operator+(fraction f)
+        {
+            fraction temp;
+            if((denominator==f.denominator)&&(denominator!=0 && f.denominator!=0))
+            {
+                temp.numerator = numerator + f.numerator;
+                temp.denominator = denominator;
+                return temp;
+            }
+            else if(denominator!=0 && f.denominator!=0)
+            {
+                temp.denominator = denominator * f.denominator;
+                temp.numerator = (numerator*f.denominator)+(f.numerator*denominator);
+                return temp;
+            }
+            else
+                cout<<"zero division error make sure you don't divide by zero."<<endl;
+            return temp;
+        }
 };
 
 int main()
 {
-    fraction f1(3,4);
-    f1.show_data();
+    fraction f1(3,4),f2(0,2),f3;
+    f3 = f1+f2;
+    f3.show_data();
     return 0;
 }
